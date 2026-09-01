ff18b7f9bec432b4dd3a93334d61a98573d8337f
AnkurOnGitHub <ankur9810230701@gmail.com>
2026-09-01 17:30:01 +0530

    Applied Caching using RedisCacheManager

--- src/main/java/com/example/studentapi/StudentApiApplication.java
+++ src/main/java/com/example/studentapi/StudentApiApplication.java
@@ -2,7 +2,9 @@ package com.example.studentapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class StudentApiApplication {
 
--- src/main/java/com/example/studentapi/service/implementation/StudentServiceImpl.java
+++ src/main/java/com/example/studentapi/service/implementation/StudentServiceImpl.java
@@ -7,7 +7,11 @@ import com.example.studentapi.repository.StudentRepository;
 import com.example.studentapi.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import tools.jackson.core.type.TypeReference;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -20,34 +24,47 @@ public class StudentServiceImpl implements StudentService {
     //using model Mapper convert studentDto to Student[Entity] vice versa
 
     @Override
+    @Cacheable(value = "students")
     public List<StudentDto> getAllStudent() {
-
+//        String key = "student-list";
+//        List<StudentDto> cachedStudent = redisService.get(key, new TypeReference<List<StudentDto>>() {});
+//        if(cachedStudent != null){
+//            System.out.println("List Found in cache");
+//            return cachedStudent;
+//        }
         List<Student>list = studentRepository.findAll();
+//        redisService.set(key,list,300L);
         return list.stream().map(s->modelMapper.map(s,StudentDto.class)).toList();
     }
+
     @Override
-    public StudentDto addStudent(AddStudentRequestDto student) {
-        Student student1 = modelMapper.map(student,Student.class);
-        Student s = studentRepository.save(student1);
-        return modelMapper.map(s,StudentDto.class);
-    }
-    //***********************************redis************************************
-    @Override
+    @Cacheable(value = "students",key = "#id")
     public StudentDto getStudentById(Long id) {
-        StudentDto studentDto = redisService.get("student_id" + id, StudentDto.class);
-        if(studentDto != null){
-            System.out.println("found in cache");
-            return studentDto;
-        }
-        else{
+//        StudentDto studentDto = redisService.get("student_id" + id, StudentDto.class);
+//        if(studentDto != null){
+//            System.out.println("found in cache");
+//            return studentDto;
+//        }
+//        else{
             Student student = studentRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Student not found..."));
             final StudentDto studentDto1 = modelMapper.map(student, StudentDto.class);
-            redisService.set("student_id" + id,studentDto1,60L);
-            return studentDto;
-        }
+//            redisService.set("student_id" + id,studentDto1,60L);
+            return studentDto1;
+//        }
 
 
     }
+
+    @Override
+    @CacheEvict(value = "students",allEntries = true)
+    public StudentDto addStudent(AddStudentRequestDto student) {
+        String key = "student-list";
+        Student student1 = modelMapper.map(student,Student.class);
+        Student s = studentRepository.save(student1);
+//        redisService.remove(key);
+        return modelMapper.map(s,StudentDto.class);
+    }
+
 
     @Override
     public StudentDto updateStudent(Long id, AddStudentRequestDto addStudentRequestDto) {
